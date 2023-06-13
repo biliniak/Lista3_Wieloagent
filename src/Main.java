f8024a80e6ecb07c3dce48dbf7b9687e980441e9
biliniak <dom.bilinski@gmail.com>
2023-06-13 17:40:21 +0200

    sadge

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,214 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.apache.commons.lang3.RandomStringUtils;
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
+public class Main extends JFrame {
+    private static SwingUtilities EventQueue;
+    private JButton startButton;
+    private JComboBox<String> comboBox1;
+    private JComboBox<String> comboBox2;
+    private JComboBox<String> comboBox3;
+    private JComboBox<String> comboBox4;
+    private JComboBox<String> comboBox5;
+    private JComboBox<String> comboBox6;
+    private JComboBox<String> comboBox7;
+    private JComboBox<String> comboBox8;
+    private JComboBox<String> comboBox9;
+    private JComboBox<String> comboBox10;
+    private JComboBox<String> comboBox11;
+    private JComboBox<String> comboBox12;
+    private JLabel Typkuchni;
+    private JLabel RodzajPosiłku;
+    private JLabel RodzajPotrawy;
+    private JLabel SmakPotrawy;
+    private JLabel SposóbPrzyrządzania;
+    private JLabel CzasPrzygotowania;
+    private JPanel FormParams;
+    private JFormattedTextField LagentowK;
+    private JTextPane Wyniki;
+    private JButton zapiszWyborButton;
+    private JTextField textField1;
+
+    private static final String INITIALIZATION_FILE = "ilosc_agentow.txt";
+    private static final String NUM_AGENTS_KEY = "numAgents";
+    private static final String JSON_FILE = "tablice.json";
+    private static final String FLAG_FILE = "flaga.txt";
+    private boolean initialized = false;
+
+    public String typ1, rodzaj1, potrawa1, smak1, sposob1, czas1;
+    public int int_agentowk;
+    public String prio_typ1, prio_rodzaj1, prio_potrawa1, prio_smak1, prio_sposob1, prio_czas1;
+
+    public String typ_kuchniii[] = {"Kuchnia Włoska",
+            "Kuchnia Polska",
+            "Kuchnia Indyjska",
+            "Kuchnia Wietnamska",
+            "Meksykańska"};
+
+    public String r_poislkuuu[] = {"Śniadanie",
+            "Lunch",
+            "Obiad",
+            "Kolacja",
+            "Dodatek",
+            "Deser"};
+    public String r_potrawyyy[] = {
+            "Sałatka",
+            "Zupa",
+            "Dania mięsne",
+            "Makaron",
+            "Pizza",
+            "Deser"};
+
+    public String smak_potrawyyy[] = {
+            "Słodki",
+            "Słony",
+            "Kwaśny",
+            "Gorzki",
+            "Pikantny",
+            "Słodko-kwaśny",
+            "Bardzo pikantny"};
+
+    public String sposob_przyrzadzeniaaa[] = {
+            "Gotowanie", "Smażenie", "Pieczenie", "Grillowanie", "Bez obróbki termicznej"
+    };
+    public String czas_przygotowaniaaa[] = {
+            "Poniżej 10 min",
+            "10-15 min",
+            "15-30 min",
+            "30-1h",
+            "1h-1,5h",
+            "Powyżej 1,5h"
+    };
+
+
+    public Main() {
+        super("Parametry wieloagentowe");
+        this.setContentPane(this.FormParams);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.pack();
+
+        LagentowK.setText("Liczba agentów (k):");
+        comboBox1.setModel(new DefaultComboBoxModel<>(typ_kuchniii));
+        comboBox2.setModel(new DefaultComboBoxModel<>(r_poislkuuu));
+        comboBox3.setModel(new DefaultComboBoxModel<>(r_potrawyyy));
+        comboBox4.setModel(new DefaultComboBoxModel<>(smak_potrawyyy));
+        comboBox5.setModel(new DefaultComboBoxModel<>(sposob_przyrzadzeniaaa));
+        comboBox6.setModel(new DefaultComboBoxModel<>(czas_przygotowaniaaa));
+
+        startButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (!initialized) {
+                    initialize();
+                    initialized = true;
+                }
+                String typ = comboBox1.getSelectedItem().toString();
+                String rodzaj = comboBox2.getSelectedItem().toString();
+                String potrawa = comboBox3.getSelectedItem().toString();
+                String smak = comboBox4.getSelectedItem().toString();
+                String sposob = comboBox5.getSelectedItem().toString();
+                String czas = comboBox6.getSelectedItem().toString();
+
+                typ1 = typ;
+                rodzaj1 = rodzaj;
+                potrawa1 = potrawa;
+                smak1 = smak;
+                sposob1 = sposob;
+                czas1 = czas;
+
+                StringBuilder wynik = new StringBuilder();
+                wynik.append("Wybrane parametry:").append("\n");
+                wynik.append("Typ kuchni: ").append(typ1).append("\n");
+                wynik.append("Rodzaj posiłku: ").append(rodzaj1).append("\n");
+                wynik.append("Rodzaj potrawy: ").append(potrawa1).append("\n");
+                wynik.append("Smak potrawy: ").append(smak1).append("\n");
+                wynik.append("Sposób przyrządzania: ").append(sposob1).append("\n");
+                wynik.append("Czas przygotowania: ").append(czas1).append("\n");
+
+                Wyniki.setText(wynik.toString());
+            }
+        });
+
+        zapiszWyborButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int_agentowk = Integer.parseInt(LagentowK.getText());
+                System.out.println(int_agentowk);
+
+                String jsonString = generateJsonString();
+                writeJsonToFile(jsonString, JSON_FILE);
+
+                writeFlagFile();
+
+                JOptionPane.showMessageDialog(null, "Wybór zapisany pomyślnie!");
+            }
+        });
+    }
+
+    private void initialize() {
+        String numAgentsString = readInitializationFile(INITIALIZATION_FILE);
+        if (numAgentsString != null) {
+            LagentowK.setText(numAgentsString);
+            int_agentowk = Integer.parseInt(numAgentsString);
         }
+    }
+
+    private String readInitializationFile(String fileName) {
+        try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            return br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private void writeJsonToFile(String jsonString, String fileName) {
+        try (FileWriter writer = new FileWriter(fileName)) {
+            writer.write(jsonString);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeFlagFile() {
+        try (FileWriter writer = new FileWriter(FLAG_FILE)) {
+            writer.write(RandomStringUtils.randomAlphabetic(10));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private String generateJsonString() {
+        List<String> paramsList = new ArrayList<>(Arrays.asList(typ1, rodzaj1, potrawa1, smak1, sposob1, czas1));
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+        String jsonString = null;
+        try {
+            jsonString = objectMapper.writeValueAsString(paramsList);
+        } catch (JsonProcessingException e) {
+            e.printStackTrace();
+        }
+        return jsonString;
+    }
+
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                JFrame frame = new Main();
+                frame.setVisible(true);
+            }
+        });
     }
 }
--- src/ParametryWieloagent.java
+++ src/ParametryWieloagent.java
@@ -1,6 +1,13 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.lang3.RandomStringUtils;
+
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.*;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -9,18 +16,18 @@ import java.util.Random;
 
 public class ParametryWieloagent extends JFrame {
     private JButton startButton;
-    private JComboBox comboBox1;
-    private JComboBox comboBox2;
-    private JComboBox comboBox3;
-    private JComboBox comboBox4;
-    private JComboBox comboBox5;
-    private JComboBox comboBox6;
-    private JComboBox comboBox7;
-    private JComboBox comboBox8;
-    private JComboBox comboBox9;
-    private JComboBox comboBox10;
-    private JComboBox comboBox11;
-    private JComboBox comboBox12;
+    private JComboBox<String> comboBox1;
+    private JComboBox<String> comboBox2;
+    private JComboBox<String> comboBox3;
+    private JComboBox<String> comboBox4;
+    private JComboBox<String> comboBox5;
+    private JComboBox<String> comboBox6;
+    private JComboBox<String> comboBox7;
+    private JComboBox<String> comboBox8;
+    private JComboBox<String> comboBox9;
+    private JComboBox<String> comboBox10;
+    private JComboBox<String> comboBox11;
+    private JComboBox<String> comboBox12;
     private JLabel Typkuchni;
     private JLabel RodzajPosiłku;
     private JLabel RodzajPotrawy;
@@ -29,69 +36,84 @@ public class ParametryWieloagent extends JFrame {
     private JLabel CzasPrzygotowania;
     private JPanel FormParams;
     private JFormattedTextField LagentowK;
-    private JFormattedTextField LagentowS;
     private JTextPane Wyniki;
     private JButton zapiszWyborButton;
     private JTextField textField1;
 
+    private static final String INITIALIZATION_FILE = "ilosc_agentow.txt";
+    private static final String NUM_AGENTS_KEY = "numAgents";
+    private static final String JSON_FILE = "tablice.json";
+    private static final String FLAG_FILE = "flaga.txt";
+    private boolean initialized = false;
 
     public String typ1, rodzaj1, potrawa1, smak1, sposob1, czas1;
-
-    public int int_agentowk, int_agentows;
+    public int int_agentowk;
     public String prio_typ1, prio_rodzaj1, prio_potrawa1, prio_smak1, prio_sposob1, prio_czas1;
 
+    public String typ_kuchniii[] = {"Kuchnia Włoska",
+            "Kuchnia Polska",
+            "Kuchnia Indyjska",
+            "Kuchnia Wietnamska",
+            "Meksykańska"};
 
-        public String typ_kuchniii[] = {"Kuchnia Włoska",
-                "Kuchnia Polska",
-                "Kuchnia Indyjska",
-                "Kuchnia Wietnamska",
-                "Meksykańska"};
+    public String r_poislkuuu[] = {"Śniadanie",
+            "Lunch",
+            "Obiad",
+            "Kolacja",
+            "Dodatek",
+            "Deser"};
+    public String r_potrawyyy[] = {
+            "Sałatka",
+            "Zupa",
+            "Dania miesne",
+            "Makaron",
+            "Pizza",
+            "Deser"};
 
-        public String r_poislkuuu[] = {"Śniadanie",
-                "Lunch",
-                "Obiad",
-                "Kolacja",
-                "Dodatek",
-                "Deser"};
-        public String r_potrawyyy[] = {
-                "Sałatka",
-                "Zupa",
-                "Dania miesne",
-                "Makaron",
-                "Pizza",
-                "Deser"};
+    public String smak_potrawyyy[] = {
+            "Słodki",
+            "Słony",
+            "Kwaśny",
+            "Gorzki",
+            "Pikantny",
+            "Słodko-kwaśny",
+            "Bardzo pikantny"};
 
-        public String smak_potrawyyy[] = {
-                "Słodki",
-                "Słony",
-                "Kwaśny",
-                "Gorzki",
-                "Pikantny",
-                "Słodko-kwaśny",
-                "Bardzo pikantny"};
-
-        public String sposob_przyrzadzeniaaa[] = {
-                "Gotowanie","Smażenie","Pieczenie","Grillowanie","Bez obróbki termicznej"
-        };
-        public String czas_przygotowaniaaa[] = {
-                "Poniżej 10 min",
-                "10-15 min",
-                "15-30 min",
-                "30-1h",
-                "1h-1,5h",
-                "Powyżej 1,5h "
-        };
+    public String sposob_przyrzadzeniaaa[] = {
+            "Gotowanie", "Smażenie", "Pieczenie", "Grillowanie", "Bez obróbki termicznej"
+    };
+    public String czas_przygotowaniaaa[] = {
+            "Poniżej 10 min",
+            "10-15 min",
+            "15-30 min",
+            "30-1h",
+            "1h-1,5h",
+            "Powyżej 1,5h "
+    };
+    public int cena[] = {
+            10,
+            25,
+            35,
+            45,
+            55,
+            65,
+    };
     Random random = new Random();
-    List<List<String>> ar = new ArrayList<List<String>>();
+    List<List<List<String>>> ar = new ArrayList<List<List<String>>>();
 
+    private void serializeToJsonFile() {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
 
-
-
+        try {
+            mapper.writeValue(new File(JSON_FILE), ar);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     public class AgentKlient {
-
         String[] tab_wart;
-
         String[] tab_params;
 
         public AgentKlient(String[] tab_wart, String[] tab_params) {
@@ -99,17 +121,18 @@ public class ParametryWieloagent extends JFrame {
             this.tab_params = tab_params;
         }
 
-        public String[] ret1(){
+        public String[] ret1() {
             return tab_wart;
         }
-        public String[] ret2(){
+
+        public String[] ret2() {
             return tab_params;
         }
     }
 
-    public class AgentSeller  extends AgentKlient{
+    public class AgentSeller extends AgentKlient {
         public AgentSeller(String[] tab_wart, String[] tab_params) {
-                super(tab_wart,tab_params);
+            super(tab_wart, tab_params);
         }
 
         @Override
@@ -121,46 +144,123 @@ public class ParametryWieloagent extends JFrame {
         public String[] ret2() {
             return tab_params;
         }
+    }
 
-        public void randomix(){
-            ar.clear();
-            for(int i = 0; i <= int_agentowk; i++) {
-                List<String> tempList = new ArrayList<String>();
-                ar.add(tempList);
-                byte[] array = new byte[7]; // length is bounded by 7
-                random.nextBytes(array);
-                String generatedString = new String(array, Charset.forName("UTF-8"));
-                int index1 = random.nextInt(typ_kuchniii.length);
-                int index2 = random.nextInt(r_poislkuuu.length);
-                int index3 = random.nextInt(r_potrawyyy.length);
-                int index4 = random.nextInt(smak_potrawyyy.length);
-                int index5 = random.nextInt(sposob_przyrzadzeniaaa.length);
-                int index6 = random.nextInt(czas_przygotowaniaaa.length);
+    public void randomix() {
+        ar.clear();
+        List<List<String>> tempList = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            List<String> innerList = new ArrayList<>();
+            byte[] array = new byte[7]; // length is bounded by 7
+            random.nextBytes(array);
+            String generatedString = RandomStringUtils.randomAlphabetic(7);
+            int index1 = random.nextInt(typ_kuchniii.length);
+            int index2 = random.nextInt(r_poislkuuu.length);
+            int index3 = random.nextInt(r_potrawyyy.length);
+            int index4 = random.nextInt(smak_potrawyyy.length);
+            int index5 = random.nextInt(sposob_przyrzadzeniaaa.length);
+            int index6 = random.nextInt(czas_przygotowaniaaa.length);
+            int index7 = random.nextInt(cena.length);
 
-                tempList.add(generatedString);
-                tempList.add(typ_kuchniii[index1]);
-                tempList.add(r_poislkuuu[index2]);
-                tempList.add(r_potrawyyy[index3]);
-                tempList.add(smak_potrawyyy[index4]);
-                tempList.add(sposob_przyrzadzeniaaa[index5]);
-                tempList.add(czas_przygotowaniaaa[index6]);
+            innerList.add(generatedString);
+            innerList.add(typ_kuchniii[index1]);
+            innerList.add(r_poislkuuu[index2]);
+            innerList.add(r_potrawyyy[index3]);
+            innerList.add(smak_potrawyyy[index4]);
+            innerList.add(sposob_przyrzadzeniaaa[index5]);
+            innerList.add(czas_przygotowaniaaa[index6]);
+            innerList.add(String.valueOf(cena[index7]));
+            tempList.add(innerList);
+        }
 
+        Random random = new Random();
+        for (int i = 0; i < int_agentowk; i++) {
+            List<List<String>> innerList = new ArrayList<>();
+            for (int j = 0; j < 15; j++) {
+                int index = random.nextInt(tempList.size());
+                List<String> agentInnerList = new ArrayList<>(tempList.get(index));
+
+                int index7 = random.nextInt(cena.length);
+                int modifiedCena = (int) (cena[index7] * (0.9 + random.nextDouble() * 0.2));
+                agentInnerList.set(7, String.valueOf(modifiedCena));
+
+                innerList.add(agentInnerList);
             }
+            ar.add(innerList);
+        }
+        initialized = true;
+        writeFlagFile(true);
+        serializeToJsonFile();
+    }
+
+    private void initializeData() {
+        if (!isFlagFileExists()) {
+            randomix();
+        }try {
+            // Odczytaj zawartość pliku "tablice.json"
+            ObjectMapper mapper = new ObjectMapper();
+            List<List<List<String>>> jsonData = mapper.readValue(new File(JSON_FILE), new TypeReference<List<List<List<String>>>>() {});
+
+            // Wyświetl zawartość pliku w konsoli
+            System.out.println("Zawartość pliku tablice.json:");
+            for (List<List<String>> innerList : jsonData) {
+                for (List<String> agentInnerList : innerList) {
+                    System.out.println(agentInnerList);
+                }
+                System.out.println();
+            }
+
+            // Przypisz odczytane dane do zmiennej "ar"
+            ar = jsonData;
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    public ParametryWieloagent() {
+    private boolean isFlagFileExists() {
+        File file = new File(FLAG_FILE);
+        return file.exists();
+    }
+
+    private void writeFlagFile(boolean value) {
+        try {
+            FileWriter writer = new FileWriter(FLAG_FILE);
+            writer.write(String.valueOf(value));
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private boolean readFlagFile() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(FLAG_FILE));
+            String line = reader.readLine();
+            reader.close();
+            return Boolean.parseBoolean(line);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public ParametryWieloagent(int int_agentowk) {
+
+        this.int_agentowk = int_agentowk;
+        initialized = readFlagFile();
+        if (!initialized) {
+            randomix();
+        }
+        initializeData();
         setContentPane(FormParams);
         setSize(700, 700);
         setTitle("System polecający dania");
         setVisible(true);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 
-
         startButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-
                 typ1 = comboBox1.getSelectedItem().toString();
                 rodzaj1 = comboBox2.getSelectedItem().toString();
                 potrawa1 = comboBox3.getSelectedItem().toString();
@@ -180,26 +280,53 @@ public class ParametryWieloagent extends JFrame {
                 System.out.println(Arrays.toString(tab_prio));
 
                 AgentSeller classObj = new AgentSeller(tab_wart, tab_prio);
-                classObj.randomix();
-                AgentKlient kliencik = new AgentKlient(tab_wart, tab_prio);
-
-
-                int_agentows = Integer.parseInt(LagentowS.getText());
-                System.out.println(int_agentows);
-
-                int_agentowk = Integer.parseInt(LagentowK.getText());
-                System.out.println(int_agentowk);
 
                 ar.forEach(System.out::println);
                 Wyniki.setText("");
                 Wyniki.setText("" + ar);
-
             }
         });
     }
 
     public static void main(String[] args) {
-        ParametryWieloagent FormParams = new ParametryWieloagent();
+        int int_agentowk = readInitializationValue();
+
+        if (int_agentowk == -1) {
+            String input = JOptionPane.showInputDialog("Podaj wartość int_agentowk:");
+            int_agentowk = Integer.parseInt(input);
+            saveInitializationValue(int_agentowk);
+        }
+
+        ParametryWieloagent FormParams = new ParametryWieloagent(int_agentowk);
+    }
+
+    private static int readInitializationValue() {
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(INITIALIZATION_FILE));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                if (line.startsWith(NUM_AGENTS_KEY)) {
+                    String[] parts = line.split("=");
+                    if (parts.length == 2) {
+                        return Integer.parseInt(parts[1]);
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return -1;
+    }
+
+    private static void saveInitializationValue(int value) {
+        try {
+            FileWriter writer = new FileWriter(INITIALIZATION_FILE);
+            writer.write(NUM_AGENTS_KEY + "=" + value);
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
     private void createUIComponents() {
